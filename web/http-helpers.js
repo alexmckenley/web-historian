e3ef4bad300232f92f5a0bd4d2a6d30e892ca0c3
Hack Reactor Students <alexmckenley@gmail.com>
2014-01-29 15:40:58 -0800

    Serving assets and archives

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -11,30 +11,37 @@ exports.headers = headers = {
   'Content-Type': "text/html"
 };
 
-exports.sendResponse = function(req, res) {
+exports.getRequest = function(req, res) {
   // console.log(req);
   var path = url.parse(req.url).pathname;
 
   if(path === "/") {
-    path = "/index.html";
+    path = archive.paths["siteAssets"] + "/index.html";
+  } else {
+    path = archive.paths["archivedSites"] + path;
   }
-  exports.serveAssets(res, archive.paths["siteAssets"] + path);
+  exports.serveAssets(res, path);
 };
 
 exports.serveAssets = function(res, asset) {
   fs.readFile(asset, 'utf8', function(err, data){
     if (err){
-      console.log("Error reading file: ", asset);
+      exports.sendResponse(res, null, 404);
     } else {
-      res.end(data);
+      exports.sendResponse(res, data, 200);
     }
   });
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
 };
 
+exports.sendResponse = function(res, data, statusCode) {
+  res.writeHead(statusCode, exports.headers);
+  res.end(data);
+};
+
 exports.methods = {
-  'GET' : exports.sendResponse
+  'GET' : exports.getRequest
 };
 
 // As you progress, keep thinking about what helper functions you can put here!
